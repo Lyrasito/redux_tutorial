71c2ef1afcd63f88322b9c26986d04b8aced0b4f
Marie G <mariegoudy@gmail.com>
2020-11-09 10:59:00 -0500

    projects slice

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import configureStore from "./store/configureStore";
-import * as actions from "./store/bugs";
+import * as actions from "./store/projects";
 
 const store = configureStore();
 
+store.dispatch(actions.projectAdded({ name: "project 1" }));
+console.log(store.getState());
+
+/*
 const unsubscribe = store.subscribe(() => {
   console.log("store changed", store.getState());
 });
@@ -16,3 +20,4 @@ store.dispatch(actions.bugResolved({ id: 1 }));
 //store.dispatch(bugRemoved(1))
 console.log(store.getState());
 console.log("Hello World!");
+*/
